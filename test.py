8dbb4cec6cb552e20cb10bb5ecd8d00d60550756
Даша <soue@ya.ru>
2022-06-04 23:07:21 +0500

    все верно

--- test.py
+++ test.py
@@ -1,17 +1,16 @@
-def verify():
-    pass
+def is_isolate(matrix, a, b):
+    flag = True
+    if matrix[a][b] + matrix[a + 1][b] + matrix[a][b + 1] + matrix[a + 1][b + 1] > 1:
+        flag = False
+    return flag
 
 
-def is_isolate():
-    pass
-
-flag = True
-for i in range(dlina - 1):
-    for j in range(dlina - 1):
-        if lst_in[i][j] + lst_in[i+1][j] + lst_in[i][j+1] + lst_in[i+1][j+1] > 1:
-            flag = False
-            break
-        if not flag:
-            break
-    if not flag:
-        break
+def verify(matrix):
+    flag = True
+    for i in range(len(matrix) - 1):
+        for j in range(len(matrix) - 1):
+            if matrix[i][j] == 1:
+                flag = is_isolate(matrix, i, j)
+            if flag == False:
+                break
+    return flag
